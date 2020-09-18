eebc735def0475cb02e8e3f8e752d471bb979919
mauricio triana velandia <951mauricetriana@gmail.com>
2020-09-18 13:01:29 -0500

    implementacion rutas servicios cliente dinamico

--- sorteos-api-master/src/routes/index.js
+++ sorteos-api-master/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getRedenciones, getRedencionId, createRedencion, updateRedencion, deleteRedencion, getSorteoActivo, getMaximoBoletas, getContratos, generarOTP, validarOtp, generarPagare, validatePagare, validaFactura, getCupoCliente, registroCliente } = require('../controllers/index.controller');
+const { getRedenciones, getRedencionId, createRedencion, updateRedencion, deleteRedencion, getSorteoActivo, getMaximoBoletas, getContratos, generarOTP, validarOtp, generarPagare, validatePagare, validaFactura, getCupoCliente, registroCliente, getContratosCliente, setContratosCliente } = require('../controllers/index.controller');
 
 router.get('/redenciones', getRedenciones);
 router.get('/redenciones/:id', getRedencionId);
@@ -22,5 +22,7 @@ router.post("/gestion-pagare-desmaterializado/v1.0/pagare/firmar",validatePagare
 router.post("/gestion-cliente-gas/v1.0/clientegas/factura",validaFactura);
 router.post("/gestion-cliente-gas/v1.0/clientegas/contrato/cupo",getCupoCliente);
 router.post("/gestion-enrolamiento/v1.0/gestion/enrolamiento",registroCliente);
+router.get("/gestion-enrolamiento/v1.0/gestion/contratos-cliente",getContratosCliente);
+router.post("/gestion-enrolamiento/v1.0/gestion/crear-contratos-cliente",setContratosCliente);
 
 module.exports = router;
